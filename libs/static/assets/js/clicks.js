01c384491896c165d9e6b7093fe4f3cd1c9d91cd
Mortexe <paris.chrysanthou@gmail.com>
2017-09-15 20:43:50 +0300

    modal animation

--- libs/static/assets/js/clicks.js
+++ libs/static/assets/js/clicks.js
@@ -10,4 +10,22 @@ $("body").on('click', '.pointer svg', function() {
     }, 1000);
 });
 
-$('#button-top h3').prowl();
+
+
+var btn = document.getElementById('button-top');
+var modal = document.getElementById('mymodal');
+
+$('body').on('click','#button-top', function (){
+  modal.style.display = "block"
+});
+
+
+var btnRadius = $('.cd-modal-bg').width()/2,
+	left = $('.cd-modal-bg').offset().left + btnRadius,
+	top = $('.cd-modal-bg').offset().top + btnRadius - $(window).scrollTop(),
+	scale = scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
+
+function scaleValue( topValue, leftValue, radiusValue, windowW, windowH) {
+	var maxDistHor = ( leftValue > windowW/2) ? leftValue : (windowW - leftValue),
+		maxDistVert = ( topValue > windowH/2) ? topValue : (windowH - topValue);
+	return Math.ceil(Math.sqrt( Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2) )/radiusValue);
