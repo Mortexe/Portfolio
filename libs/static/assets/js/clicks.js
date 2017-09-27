db64242e8b208ff4e153c90f167fd116633c9775
Mortexe <paris.chrysanthou@gmail.com>
2017-09-27 15:00:35 +0300

    top text fadeOut on scroll

--- libs/static/assets/js/clicks.js
+++ libs/static/assets/js/clicks.js
@@ -1,37 +1,3 @@
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header .nav-bar').outerHeight();
-
-$(window).scroll(function(event){
-  didScroll = true;
-});
-
-setInterval(function(){
-  if(didScroll){
-    hasScrolled();
-    didScroll = false;
-  }
-}, 250);
-
-function hasScrolled(){
-  var st = $(this).scrollTop();
-
-  if (Math.abs(lastScrollTop - st) <= delta)
-    return;
-
-  if (st > lastScrollTop && st > navbarHeight){
-    $('nav').removeClass('nav-bar').addClass('nav-up');
-    $('nav').css('opacity', '0');
-  } else {
-    if(st + $(window).height() < $(document).height()){
-      $('nav').removeClass('nav-up').addClass('nav-bar');
-      $('nav').css('opacity', '.7');
-    }
-  }
-  lastScrollTop = st;
-}
-
 $(window).resize(function(){
    var width = $(window).width();
    if(width<990){
@@ -43,49 +9,63 @@ $(window).resize(function(){
  })
  .resize();
 
+$(window).scroll(function(event){
+   var contTop = $(this).scrollTop();
+   var textpos = $('.top-text').offset().top;
+   if(150 < contTop){
+     $('.top-text').hide('slow');
+     $('#button-top').hide('slow')
+   }if(contTop < 151) {
+     $('.top-text').show('slow');
+    $('#button-top').show('slow')
+   }
+  if (contTop >= 650){
+    $('.nav-bar').css('background','rgb(250, 250, 250)');
+    $('.nav-bar').css('box-shadow', '0px 2px 8px  #888888');
+    $('.nav-bar ul').css('color', '#FDCD3B');
+  }if(contTop < 650){
+    $('.nav-bar').css('background', 'none');
+    $('.nav-bar').css('box-shadow', 'none');
+    $('.nav-bar ul').css('color', 'white');
+  }
+});
 
 // scroll to div on click section
 
 $("body").on('click', '.pointer-wrap a', function() {
     $('html, body').animate({
         scrollTop: $(".container").offset().top
-    }, 1000);
+    }, 700);
 });
 
 $("body").on('click', '.pointer-wrap svg', function() {
     $('html, body').animate({
-        scrollTop: $(".container").offset().top
-    }, 1000);
+        scrollTop: $(".container").offset().top - 50
+    }, 700);
 });
 
 $('body').on('click touchstart', '#button-top h4', function() {
     $('html, body').animate({
-        scrollTop: $('.email-sect').offset().top
-    }, 1500);
-});
-
-$('body').on('click touchstart', '.serv', function() {
-    $('html, body').animate({
-        scrollTop: $('.services').offset().top
-    }, 1000);
+        scrollTop: $('.email-sect').offset().top - 50
+    }, 700);
 });
 
 $('body').on('click touchstart', '.work', function() {
     $('html, body').animate({
-        scrollTop: $('.work-sect').offset().top
-    }, 1000);
+        scrollTop: $('.work-sect').offset().top - 50
+    }, 700);
 });
 
 $('body').on('click touchstart', '.about', function() {
     $('html, body').animate({
-        scrollTop: $('.about-sect').offset().top
-    }, 1000);
+        scrollTop: $('.about-sect').offset().top - 100
+    }, 700);
 });
 
 $('body').on('click touchstart', '.cont', function() {
     $('html, body').animate({
-        scrollTop: $('.email-sect').offset().top
-    }, 1000);
+        scrollTop: $('.email-sect').offset().top - 100
+    }, 700);
 });
 
   //hide scroll on modal
